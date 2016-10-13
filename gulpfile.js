e13a56ebc83022acc18263f534ed2ced39ef0cf5
Sandro Kock <sandro.kock@gmail.com>
2016-10-13 12:55:34 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict'
+const gulp = require('gulp')
+
+gulp.task('generate-service-worker', (callback) => {
+  let path = require('path')
+  let swPrecache = require('sw-precache')
+  let rootDir = 'app'
+
+  swPrecache.write(path.join(rootDir, 'service-worker.js'), {
+    staticFileGlobs: [ rootDir + '/*.{html, png}', rootDir + '/elements/*.{js,html,png}', rootDir + '/bower_components/*/*.{js,html,png}', '/bower_components/*/!(Gruntfile.js)' ],
+    stripPrefix: rootDir
+  }, callback)
+})
